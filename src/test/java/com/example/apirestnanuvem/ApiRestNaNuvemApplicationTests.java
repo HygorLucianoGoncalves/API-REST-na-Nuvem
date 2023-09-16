35b48990dc15697cb959ab4851598a27f878bcb7
Hygor Luciano <hygorluciano0@gmail.com>
2023-09-16 13:55:50 -0300

    first commit

---
+++ src/test/java/com/example/apirestnanuvem/ApiRestNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestnanuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNaNuvemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
